8539688ad150b214c6ee509599daacda1977dc7d
kitonyi <kitonyi@gmail.com>
2023-11-03 06:11:07 +0300

    Added user signup

--- backend/account/urls.py
+++ backend/account/urls.py
@@ -3,10 +3,12 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from . import api
 
 urlpatterns = [
     
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('signup/', api.signup, name='signup')
     
 ]
